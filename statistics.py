0b41e6460520954dd5d2c10be9dd27ac67ffd59f
Chandramohan_Naren <90902867+Chandramohan-Naren@users.noreply.github.com>
2021-10-24 19:14:38 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,6 +1,3 @@
 
 def calculateStats(numbers):
-    if numbers == []:
-        return float("nan")
-    return{'avg': sum(numbers)/len(numbers), "max": max(numbers),
-           "min": min(numbers)}
+  return None
